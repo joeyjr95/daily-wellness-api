16c3452cffa3b73500dc26d79d76a974fcdf974a
Joey Romo <joeyjr95@gmail.com>
2019-10-25 15:07:00 -0400

    changes

--- src/app.js
+++ src/app.js
@@ -19,16 +19,6 @@ app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
 app.use(cors());
 app.use(helmet());
 
-app.use(function validateBearerToken(req,res,next){
-  const authToken = req.get('Authorization');
-  const apiToken = process.env.API_TOKEN;
-
-  if (!authToken || authToken.split(' ')[1] !== apiToken) {
-    return res.status(401).json({ error: 'Unauthorized request' });
-  }
-
-  next();
-});
 
 app.use('/api/auth', authRouter);
 app.use('/api/users', usersRouter);
