7614dddee3c418757a4062e54e4a905a5583e941
Joey Romo <joeyjr95@gmail.com>
2019-10-25 15:12:34 -0400

    changes

--- src/app.js
+++ src/app.js
@@ -27,6 +27,7 @@ app.use('/api/averages', averageRouter);
 
 app.use(function errorHandler(error, req, res, next){
   let response;
+  console.log(error);
   if (NODE_ENV === 'production') {
     response = { error: {message: 'server error'} };
   } else {
